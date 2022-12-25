eef7dfce56455059c1445311e262d4abbd48d69a
Thamognya Kodi <contact@thamognya.com>
2022-12-25 10:04:11 +0700

    update

--- research/Internet-NLP/paper/compile.sh
+++ research/Internet-NLP/paper/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+bibtex-tidy --curly --numeric --tab --align=13 --duplicates=key --no-escape --no-remove-dupe-fields ./ref.bib && pdflatex main
 pdflatex main
 bibtex main
 pdflatex main
